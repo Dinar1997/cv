787653b4041e8f9e3831eccc2669ec4efd79eaeb
Dinar1997 <dinar140597@gmail.com>
2024-12-29 15:06:31 +0500

    Add files via upload

---
+++ login.cy.js
@@ -0,0 +1,78 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный пароль и верный логин', function () {
+         cy.visit('https://login.qa.studio/');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');     
+     })
+
+     it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#forgotForm > .header');
+        cy.get('#mailForgot').type('german@dolnikov.ru')
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');   
+    })
+
+    it('Неверный пароль и верный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');     
+    })
+
+    it('Верный пароль и неверный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@olnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');     
+    })
+
+    it('Ошибка валидации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');     
+    })
+
+    it('Верный пароль и верный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');     
+    })
+
+    it('Верный пароль и верный логин', function () {
+        cy.visit('https://pokemonbattle.ru/login');
+        cy.get(':nth-child(1) > .auth__input').type('USER_LOGIN');
+        cy.get('#password').type('USER_PASSWORD');
+        cy.get('.auth__button').click();
+        cy.wait(2000);
+        cy.get('.pokemon__title').contains('Покемоны');
+        cy.get('.header__container > .header__id').click();
+        cy.get('[href="/shop"]').click();
+        cy.get(':nth-child(3) > .shop__button').click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('NAME');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__font-for-success').contains('Покупка прошла успешно');     
+    })
+ })
+ 
